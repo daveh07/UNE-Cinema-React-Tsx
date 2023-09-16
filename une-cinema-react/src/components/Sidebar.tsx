ed206c0018edc1df4e9a932c443716d4421b0b9b
DavidH <david.hill87@outlook.com>
2023-09-16 22:53:33 +1000

    Added CSS for SideNav

--- une-cinema-react/src/components/Sidebar.tsx
+++ une-cinema-react/src/components/Sidebar.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react';
 import { FaHome, FaRegArrowAltCircleRight, FaUserEdit, FaBars } from 'react-icons/fa';
 import { NavLink } from 'react-router-dom';
+import style from './Sidebar.module.css'
 
 interface SidebarProps {
   children: ReactNode;
@@ -26,18 +27,18 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
   ];
 
   return (
-    <div className="container">
-        <div className="sidebar">
-            <div className="top_section">
-                <div className="bars">
+    <div className={style.container}>
+        <div className={style.sidebar}>
+            <div className={style.top_section}>
+                <div className={style.bars}>
                     <FaBars/>
                 </div>
             </div>
             {
                 menuItem.map((item, index)=>(
-                    <NavLink to={item.path} key={index} className="link">
-                    <div className="icon">{item.icon}</div>
-                    <div className="link_text">{item.name}</div>
+                    <NavLink to={item.path} key={index} className={style.link}>
+                    <div className={style.icon}>{item.icon}</div>
+                    <div className={style.link_text}>{item.name}</div>
                     </NavLink>
             ))
             }
